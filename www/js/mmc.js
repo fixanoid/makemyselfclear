78f1f0c6de68723e285056de26e613050294d74a
Felix <felix.shnir@gmail.com>
2012-04-14 17:36:09 -0400

    As of 5:36PM.

--- www/js/mmc.js
+++ www/js/mmc.js
@@ -1,4 +1,5 @@
 var mmc = {
+	facebookAppID: '378127265561580',
 	dictionary: {},
 	
 	runDataMatch: function(data) {
@@ -11,9 +12,103 @@ var mmc = {
 		});
 	},
 
+	addObservers: function() {
+		$('#go').click(function() {
+			mmc.openPage(2);
+		});
+
+		$('#facebook-auth').click(function(e) {
+			e.preventDefault();
+
+			var path = 'https://www.facebook.com/dialog/oauth?';
+			var queryParams = ['client_id=' + mmc.facebookAppID,
+				'redirect_uri=' + window.location,
+				'response_type=token',
+				"scope=read_stream,read_insights"
+			];
+			var query = queryParams.join('&');
+			var url = path + query;
+			window.location.href = url;
+		});
+
+		$('#policy').click(function(e) {
+			e.preventDefault();
+			mmc.openPage('extra');
+			$('#page-extra').load('privacy.html');
+		});
+	},
+
+	nada: function(e) {
+	
+	},
+
+	initFacebook: function() {
+		if (window.location.hash.length == 0) { return; }
+
+		var accessToken = window.location.hash.substring(1);
+		var path = "https://graph.facebook.com/me?";
+		var queryParams = [accessToken, 'callback=mmc.nada'];
+		var query = queryParams.join('&');
+		var url = path + query;
+
+		(function (d) {
+			var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+			if (d.getElementById(id)) {
+				return;
+			}
+			js = d.createElement('script');
+			js.id = id;
+			js.async = true;
+			js.src = "//connect.facebook.net/en_US/all.js";
+			ref.parentNode.insertBefore(js, ref);
+		}(document));
+
+		window.fbAsyncInit = function () {
+			FB.init({
+				appId: mmc.facebookAppID,
+				channelUrl:'channel.html',
+				status:true,
+				cookie:true,
+				xfbml:true,
+				oauth:true
+			});
+		}
+
+        // use jsonp to call the graph
+        var script = document.createElement('script');
+        script.src = url;
+        document.body.appendChild(script);
+
+		script.onload = function() {
+		setTimeout(function() {
+				FB.api({
+					method:'fql.query',
+					query:"SELECT post_id, actor_id, target_id, message FROM stream WHERE filter_key in (SELECT filter_key FROM stream_filter WHERE uid=me() AND type='newsfeed') AND is_hidden = 0"
+				}, function (response) {
+					var data = [];
+					$(response).each(function() {
+						var entry = $(this)[0];
+						if (entry.message) {
+							
+							entry.content = entry.message;
+							data.push(entry);
+						}
+					});
+
+					console.log(["Users posts", data]);
+					mmc.runDataMatch(data);
+				});
+			}, 2000);
+		}
+
+		mmc.openPage(3);
+	},
+
 	init: function() {
 		// load initial page
 		mmc.openPage(1);
+		mmc.addObservers();
+		mmc.initFacebook();
 		
 		// load dictionary.
 		$.ajax({
@@ -40,7 +135,7 @@ var mmc = {
 				}
 				//console.log(mmc.dictionary);
 				
-				
+/*
 				// rigging for testing.
 				var d = [];
 				d.push({'content': 'I love towlehead fagots', 'id': 123213});
@@ -49,6 +144,7 @@ var mmc = {
 				
 				// runnign test
 				mmc.runDataMatch(d);
+*/
 			}
 		});
 	},
@@ -59,4 +155,6 @@ var mmc = {
 	}
 };
 
-mmc.init();
+$(document).ready(function(){
+	mmc.init();
+});
